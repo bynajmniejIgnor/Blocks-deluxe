e2c89a8ae55dacc8632f6a7589f4ac83bab46f98
Igor Szczepaniak <igor.szczepaniak.02@gmail.com>
2022-03-29 21:15:43 +0100

    'update'

--- SDL_Tetris/main.c
+++ SDL_Tetris/main.c
@@ -54,51 +54,79 @@ void drawBackground(SDL_Renderer* renderer, SDL_Rect backgr){
 	SDL_RenderFillRect(renderer,&backgr);
 }
 
-cell board[20][10];
+cell board[22][10];
 int cellSize=40;
+
 void initBoard(SDL_Renderer* renderer, float origin[]){
-	for(int y=0; y<20; y++){
+	for(int y=0; y<22; y++){
 		for(int x=0; x<10; x++){
 			board[y][x].r.w=cellSize;
 			board[y][x].r.h=cellSize;
-			board[y][x].r.x=10+origin[0]+42*x;
-			board[y][x].r.y=10+origin[1]+42*y;
+			board[y][x].r.x=10+origin[0]+42.0f*x;
+			board[y][x].r.y=-83+10+origin[1]+42.0f*y;
 		}
 	}
 }
 void updateBoard(SDL_Renderer* renderer){
-	for(int y=0; y<20; y++){
+	for(int y=2; y<22; y++){
 		for(int x=0; x<10; x++){
 			switch(board[y][x].val){
 				case 1:
 					SDL_SetRenderDrawColor(renderer,0,255,255,255); //cyan long boi
 					break;
+				case 9:
+					SDL_SetRenderDrawColor(renderer,0,255,255,255); //cyan long boi stable
+					break;
 				case 2:
 					SDL_SetRenderDrawColor(renderer,0,0,255,255); //blue rev L
+					break;
+				case 10:
+					SDL_SetRenderDrawColor(renderer,0,0,255,255); //blue rev L stable
 					break;
 				case 3:
 					SDL_SetRenderDrawColor(renderer,255,144,0,255); //orange L
 					break;
+				case 11:
+					SDL_SetRenderDrawColor(renderer,255,144,0,255); //orange L stable
+					break;
 				case 4:
 					SDL_SetRenderDrawColor(renderer,255,255,0,255); //yellow square
+					break;
+				case 12:
+					SDL_SetRenderDrawColor(renderer,255,255,0,255); //yellow square stable
 					break;
 				case 5:
 					SDL_SetRenderDrawColor(renderer,0,255,0,255); //green zgigzag
 					break;
+				case 13:
+					SDL_SetRenderDrawColor(renderer,0,255,0,255); //green zgigzag stable
+					break;
 				case 6:
 					SDL_SetRenderDrawColor(renderer,255,0,255,255); //purple T
+					break;
+				case 14:
+					SDL_SetRenderDrawColor(renderer,255,0,255,255); //purple T stable
 					break;
 				case 7:
 					SDL_SetRenderDrawColor(renderer,255,0,0,255); //red zigzag
 					break;
+				case 15:
+					SDL_SetRenderDrawColor(renderer,255,0,0,255); //red zigzag stable
+					break;
 				case 0:
+					SDL_SetRenderDrawColor(renderer,0,0,0,255);
 					break;
 			}
 			SDL_RenderFillRect(renderer,&board[y][x].r);
 			SDL_SetRenderDrawColor(renderer,0,0,0,255);
-
 		}
 	}
+}
+
+void swap(int* a, int* b){
+	int tmp=*a;
+	*a=*b;
+	*b=tmp;
 }
 
 void spawnTetrimino(int tetrimino[4][4], int y, int x){
@@ -108,9 +136,72 @@ void spawnTetrimino(int tetrimino[4][4], int y, int x){
 		}
 	}
 }
-int main(int argc, char* args[]){
-	SDL_Init(SDL_INIT_EVERYTHING);
 
+void devMode(){
+	system("cls");
+	for(int i=0; i<22; i++){
+		printf("%d\t",i);
+		for(int j=0; j<10; j++){
+			printf(" %d ",board[i][j].val);
+		}
+		printf("\n");
+	}
+}	
+
+void settle(int y, int x){
+	if(board[y][x].val==0 || board[y][x].val>=8){
+		//printf("Returning settle cuz y=%d x=%d is 0\n",y,x);
+		return;
+	}
+	//printf("Calling settle for y=%d x=%d\n",y,x);
+	board[y][x].val+=8;
+	settle(y-1,x);
+	settle(y,x+1);
+	settle(y,x-1);
+}
+
+void gravity(){
+	for(int i=21; i>1; i--){
+		for(int j=0; j<10; j++){
+			//if(board[20][3].val==15) continue;
+			if(board[i-1][j].val>=8){
+				continue;
+			}
+			if(board[i][j].val!=0){
+				//board[i][j].val+=8;
+				settle(i,j);
+				continue;
+			}
+			board[i][j].val=board[i-1][j].val;
+			board[i-1][j].val=0;
+		}
+	}
+	for(int j=0; j<10; j++){
+		if(board[i-1][j].val!=0){
+			settle(21,j);
+		}
+	}
+	//printf("%d\n",board[20][3].val);
+}
+
+void resetBoard(){
+	for(int i=0; i<22; i++){
+		for(int j=0; j<10; j++){
+			board[i][j].val=0;
+		}
+	}
+	spawnTetrimino(longBoi,2,0);
+	spawnTetrimino(revL,2,5);
+	spawnTetrimino(L,5,1);
+	spawnTetrimino(square,7,8);
+	spawnTetrimino(revZigzag,10,0);
+	spawnTetrimino(T,12,5);
+	spawnTetrimino(zigzag,15,3);
+}
+
+int main(int argc, char* args[]){
+
+	SDL_Init(SDL_INIT_EVERYTHING);
 	int windowWidth=700;
 	int windowHeight=900;
 	SDL_Window *window=SDL_CreateWindow("Competitive blocks...",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,windowWidth,windowHeight,SDL_WINDOW_SHOWN);
@@ -130,7 +221,7 @@ int main(int argc, char* args[]){
 
 	initBoard(renderer,origin);
 
-	spawnTetrimino(longBoi,0,0);
+	spawnTetrimino(longBoi,2,0);
 	spawnTetrimino(revL,2,5);
 	spawnTetrimino(L,5,1);
 	spawnTetrimino(square,7,8);
@@ -147,10 +238,14 @@ int main(int argc, char* args[]){
 			case SDL_KEYDOWN:
 				switch(event.key.keysym.sym){
 					case SDLK_SPACE:
-						printf("Space pressed\n");
+						gravity();
+						devMode();
 						break;
 					case SDLK_ESCAPE:
 						running=0;
+						break;
+					case SDLK_r:
+						resetBoard();
 						break;
 				}
 				break;
